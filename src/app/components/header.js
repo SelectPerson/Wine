3df10ca2b31973f91dd4aa3d2f89ce37915c769a
SelectPerson <vovalutsk961@gmail.com>
2024-11-17 13:06:47 +0200

    upd

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,40 +1,43 @@
 import React from 'react';
+import IconInstagram from "@/app/components/icons/instagram";
+import IconTwitter from "@/app/components/icons/twitter";
+import IconYoutube from "@/app/components/icons/youtube";
 
 const Header = () => {
   return (
     <header className="container header">
-      <div className="header-left">
-        <nav className="header-nav">
-          <a href="#">
+      <div className="header__left">
+        <nav className="header__nav">
+          <a href="#" className="header__elem">
             About Narmada
           </a>
 
-          <a href="">
+          <a href="" className="header__elem">
             Our Wines
           </a>
 
-          <a href="#">
+          <a href="#" className="header__elem">
             FAQs
           </a>
         </nav>
       </div>
 
-      <div className="header-right">
+      <div className="header__right">
         <div className="header-icons">
-          <a href="#" target={"_blank"}>
-
+          <a className="header-icons__elem icon-border" href="#">
+            <IconInstagram/>
           </a>
 
-          <a href="#" target={"_blank"}>
-
+          <a className="header-icons__elem icon-border" href="#">
+            <IconTwitter/>
           </a>
 
-          <a href="#" target={"_blank"}>
-
+          <a className="header-icons__elem icon-border" href="#">
+            <IconYoutube/>
           </a>
         </div>
 
-        <button className="header-button">
+        <button className="header__button">
           Our Wine Club
         </button>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,16 @@ import Info from "@/app/components/info";
 import Footer from "@/app/components/footer";
 import Faq from "@/app/components/faq";
 import About from "@/app/components/about";
+import Image from "next/image";
+import upperImage from "../app/images/bg/hero-min.png";
 
 export default function Home() {
   return (
     <div className="main-page">
-      <div className="main-page__image">
-
+      <div className="main-page__upper">
+        <div className="main-page__bg">
+          <Image src={upperImage} alt={"Bg"} />
+        </div>
 
         <Header />
         <Hero />
